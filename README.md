2cab1a7ff8fe869d2c90f244a4abe0dfbf0431fe
Sean McAtee <smcatee@camsys.com>
2024-08-18 17:19:53 -0700

    Initial test setup

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # torchys
 Tinkering with Pytorch to learn how it can speed things up
+
+## Lessons Learned:
+
+ - Using the add funciton with a scale value alpha is 2x as fast as separate operations on the CPU.
+ ```
+util = torch.add(torch.tensor([const]), var, alpha=coefficient) #faster on CPU
+util = var * coeff + K # 2x slower on CPU!
---
+++ tester.py
@@ -0,0 +1,64 @@
+"""A simple test logit model with 5,000 zones and random numbers for skims"""
+
+import torch
+import numpy as np
+import timeit
+import os
+
+N_ZONES = 5000
+
+c_ivtt = -0.025
+c_ovtt = c_ivtt * 2.5
+c_cost = -0.005
+
+k_trn = -1.5
+k_hwy = -1.0
+
+torch.set_default_device('cpu')
+
+# Set environment variables for OpenMP
+os.environ["OMP_NUM_THREADS"] = "1"  # Adjust this value based on your experiments
+os.environ["MKL_NUM_THREADS"] = "1"  # Adjust this value based on your experiments
+
+torch.set_num_threads(1)
+
+def init_data():
+
+    data = {}
+    data["hwy_time"] = torch.rand(N_ZONES, N_ZONES)
+    data["trn_time"] = torch.rand(N_ZONES, N_ZONES)
+    data["trn_fare"] = torch.rand(N_ZONES, N_ZONES)
+
+    return data
+
+def hwy_util(data):
+
+    # Compute hwy utility - very simple (0.25 sec for 10 runs) - but faster on CUDA
+    hwy_util = data["hwy_time"] * c_ivtt + k_hwy
+
+    # Compute hwy utility, with the add function and alpha parameter (0.12 sec for 10 runs) - but slower on CUDA
+    # hwy_util = torch.add(torch.tensor([k_hwy]), data["hwy_time"], alpha=c_ivtt)
+
+    #print("hwy_util mean:", hwy_util.mean())
+
+def trn_util(data):
+
+    # Compute trn utility - very simple (0.5352 sec for 10 runs)
+    # trn_util = data["trn_time"] * c_ivtt + data["trn_fare"] * c_cost + k_trn
+
+    # Compute trn utility, with the add function and alpha parameter ()
+    trn_util = torch.add(torch.add(torch.tensor([k_trn]), data["trn_time"], alpha=c_ivtt), data["trn_fare"], alpha=c_cost)
+
+    # print("trn_util mean:", trn_util.mean())
+    
+
+if __name__ == '__main__':
+
+    timer_number = 10
+
+    time = timeit.timeit(setup="data = init_data()", stmt="hwy_util(data)", number=timer_number, globals=globals())
+
+
+    print(f"Time: {time}")
+
+    # print(torch.__config__.parallel_info())
