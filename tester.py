4d5e5ad7b79ef6aab3e14a2586dcfe7c12b2ccde
Sean McAtee <smcatee@camsys.com>
2024-08-19 04:53:40 -0700

    small optimizations, notes

--- tester.py
+++ tester.py
@@ -5,6 +5,14 @@ import numpy as np
 import timeit
 import os
 
+torch.set_default_device('cuda')
+
+# Set environment variables for OpenMP
+# os.environ["OMP_NUM_THREADS"] = "8"  # Adjust this value based on your experiments
+# os.environ["MKL_NUM_THREADS"] = "8"  # Adjust this value based on your experiments
+
+#torch.set_num_threads(4)
+
 N_ZONES = 5000
 
 c_ivtt = -0.025
@@ -13,14 +21,6 @@ c_cost = -0.005
 
 k_trn = -1.5
 k_hwy = -1.0
-
-torch.set_default_device('cuda')
-
-# Set environment variables for OpenMP
-# os.environ["OMP_NUM_THREADS"] = "8"  # Adjust this value based on your experiments
-# os.environ["MKL_NUM_THREADS"] = "8"  # Adjust this value based on your experiments
-
-#torch.set_num_threads(4)
 
 def init_data():
 
@@ -34,10 +34,10 @@ def init_data():
 def hwy_util(data):
 
     # Compute hwy utility - very simple (0.25 sec for 10 runs) - but faster on CUDA
-    hwy_util = data["hwy_time"] * c_ivtt + k_hwy
+    # hwy_util = data["hwy_time"] * c_ivtt + k_hwy
 
     # Compute hwy utility, with the add function and alpha parameter (0.12 sec for 10 runs) - but slower on CUDA
-    # hwy_util = torch.add(torch.tensor([k_hwy]), data["hwy_time"], alpha=c_ivtt)
+    hwy_util = torch.add(k_hwy, data["hwy_time"], alpha=c_ivtt)
 
     #print("hwy_util mean:", hwy_util.mean())
 
@@ -68,7 +68,7 @@ if __name__ == '__main__':
 
     timer_number = 10
 
-    time = timeit.timeit(setup="data = init_data()", stmt="trn_util(data)", number=timer_number, globals=globals())
+    time = timeit.timeit(setup="data = init_data()", stmt="hwy_util(data)", number=timer_number, globals=globals())
 
 
     print(f"Time: {time}")
